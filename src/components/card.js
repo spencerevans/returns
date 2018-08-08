2d5df477e2fae427c2000651dd7ee2217f2945ef
Spencer Evans <spencerevans.sce@gmail.com>
2018-08-08 15:30:32 +0300

    added more state componenents

--- src/components/card.js
+++ src/components/card.js
@@ -11,44 +11,55 @@ class Card extends Component {
             pricePast: '',
             yearsAgo: '',
             priceToday: '',
-            divYield: 2.31,
-            initialInvestment: 10000,
+            divYield: '',
+            initialInvestment: '',
             weeklyContribution: '',
-            yearsInvesting: ''
+            yearsInvesting: '',
+            reInvestDividends: ''
         }
         this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     handleInputChange(event) {
-        this.setState({ divYield: event.target.value })
+        this.setState({ [event.target.name]: event.target.value})
     }
 
     render() {
 
-        return (
-            <div className='card'>
-                <h1>{this.state.divYield}</h1>
-                <input onChange={(event) => this.handleInputChange(event)}/>
-            </div>
-        )
+        const inputData = [
+            {title: 'Price X Years Ago', state: this.state.pricePast, name: 'pricePast'},
+            {title: 'X (Years)', state: this.state.yearsAgo, name: 'yearsAgo'},
+            {title: 'Price Today', state: this.state.priceToday, name: 'priceToday'},
+            {title: 'Div/Yield', state: this.state.divYield, name: 'divYield'},
+            {title: 'Initial Investment', state: this.state.initialInvestment, name: 'initialInvestment'},
+            {title: 'Weekly Contribution', state: this.state.weeklyContribution, name: 'weeklyContribution'},
+            {title: 'Years of Investing', state: this.state.yearsInvesting, name: 'yearsInvesting'},
+            {title: 'Reinvest Dividends?', state: this.state.reInvestDividends, name: 'reInvestDividends'}
+            
+        ]
 
         return (
 
             
             <div className='card'>
-                <h1>{this.state.divYield}</h1>
-                <h1>{this.state.divYield}</h1>
-                <h1>{this.state.divYield}</h1>
-                <h1>{this.state.divYield}</h1>
-                <h1>{this.state.divYield}</h1>
-                <h1>{this.state.divYield}</h1>
-               { Input('Price X Years Ago') }
-               { Input('X (Years)') }  
-               { Input('Price Today') } 
-               { Input('Div/Yield') } 
-               { Input('Initial Investment') } 
-               { Input('Weekly Contribution') } 
-               { Input('Years of Investing')}
+            {
+                inputData.map(data => Input( (data) , this.handleInputChange ))
+            
+            }
+
+            {/* {
+                inputData.map(data => {
+                    return <div key={index}>{data.title}</div>
+                })
+            } */}
+               {/* { Input('Price X Years Ago', this.state.pricePast, this.handleInputChange, 'pricePast') }
+               { Input('X (Years)', this.state.yearsAgo, this.handleInputChange, 'yearsAgo') }
+               { Input('Price Today', this.state.priceToday, this.handleInputChange, 'priceToday') }
+               { Input('Div/Yield', this.state.divYield, this.handleInputChange, 'divYield') }
+               { Input('Initial Investment', this.state.initialInvestment, this.handleInputChange, 'initialInvestment') }
+               { Input('Weekly Contribution', this.state.weeklyContribution, this.handleInputChange, 'weeklyContribution') }
+               { Input('Years of Investing', this.state.yearsInvesting, this.handleInputChange, 'yearsInvesting') }
+               { InputBool('Reinvest Dividends?', this.state.reInvestDividends, this.handleInputChange, 'reInvestDividends') } */}
             </div>
         )
     }
--- src/components/input.js
+++ src/components/input.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Input = (title) => {
-    return(
-        <div className='input'>
-            <input />
-            <label>{title}</label>
-        </div>
-    )
+const Input = ({title, state, name}, onChange) => {
+    
+        return (
+            <div className='input' key={name}>
+                <input name={name} value={state} onChange={onChange}/>
+                <label>{title}</label>
+            </div>
+        )
 }
 
 export default Input;
